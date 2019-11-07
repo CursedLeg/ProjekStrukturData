3e984b0386ea8e14456ba423ef28b1fa76026cd7
Leguna <ahmadtuflihunxd@gmail.com>
2019-11-07 14:29:28 +0700

    ubah tulisan

--- 19_AhmadTuflihun_Jobsheet3_ShortBubbleSort.py
+++ 19_AhmadTuflihun_Jobsheet3_ShortBubbleSort.py
@@ -1,20 +1,20 @@
 ##Ahmad Tuflihun | 3201816084 | Semester 2B
 
 A= [40,30,50,60];
-proses = 0;
+process = 0;
 
 print(A);
 
 for i in range(len(A)-1,-1,-1):
 	swapped = False;
 	for j in range(0,i):
-		proses= proses + 1;
+		process= process + 1;
 		if(A[j]>A[j+1]):
 			A[j],A[j+1]=A[j+1],A[j];
 			swapped = True;
 	if(swapped==False):
 		break;
 
-print("Proses:",proses);
+print("Process:",process);
 print(A);
 input();
